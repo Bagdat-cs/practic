69b75009a9cc8b62c5ccbe94a6f4fb3be511af12
Bagdat-cs <k.bagdat006@gmail.com>
2026-03-04 12:22:46 +0600

    Update practic7.1.java

--- practic7.1.java
+++ practic7.1.java
@@ -1,238 +1,191 @@
-using System;
-using System.Collections.Generic;
+import java.util.*;
 
-namespace SmartHomeCommandPattern
-{
-    public interface ICommand
-    {
-        void Execute();
-        void Undo();
+interface Command {
+    void execute();
+    void undo();
+}
+
+class Light {
+    public void on() { System.out.println("Свет включен"); }
+    public void off() { System.out.println("Свет выключен"); }
+}
+
+class AirConditioner {
+    public void on() { System.out.println("Кондиционер включен"); }
+    public void off() { System.out.println("Кондиционер выключен"); }
+}
+
+class TV {
+    public void on() { System.out.println("Телевизор включен"); }
+    public void off() { System.out.println("Телевизор выключен"); }
+}
+
+class Curtains {
+    public void open() { System.out.println("Шторы открыты"); }
+    public void close() { System.out.println("Шторы закрыты"); }
+}
+
+class MusicPlayer {
+    public void play() { System.out.println("Музыка играет"); }
+    public void stop() { System.out.println("Музыка остановлена"); }
+}
+
+class LightOnCommand implements Command {
+    private Light light;
+    public LightOnCommand(Light light) { this.light = light; }
+    public void execute() { light.on(); }
+    public void undo() { light.off(); }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+    public LightOffCommand(Light light) { this.light = light; }
+    public void execute() { light.off(); }
+    public void undo() { light.on(); }
+}
+
+class ACOnCommand implements Command {
+    private AirConditioner ac;
+    public ACOnCommand(AirConditioner ac) { this.ac = ac; }
+    public void execute() { ac.on(); }
+    public void undo() { ac.off(); }
+}
+
+class ACOffCommand implements Command {
+    private AirConditioner ac;
+    public ACOffCommand(AirConditioner ac) { this.ac = ac; }
+    public void execute() { ac.off(); }
+    public void undo() { ac.on(); }
+}
+
+class TVOnCommand implements Command {
+    private TV tv;
+    public TVOnCommand(TV tv) { this.tv = tv; }
+    public void execute() { tv.on(); }
+    public void undo() { tv.off(); }
+}
+
+class TVOffCommand implements Command {
+    private TV tv;
+    public TVOffCommand(TV tv) { this.tv = tv; }
+    public void execute() { tv.off(); }
+    public void undo() { tv.on(); }
+}
+
+class CurtainsOpenCommand implements Command {
+    private Curtains curtains;
+    public CurtainsOpenCommand(Curtains curtains) { this.curtains = curtains; }
+    public void execute() { curtains.open(); }
+    public void undo() { curtains.close(); }
+}
+
+class CurtainsCloseCommand implements Command {
+    private Curtains curtains;
+    public CurtainsCloseCommand(Curtains curtains) { this.curtains = curtains; }
+    public void execute() { curtains.close(); }
+    public void undo() { curtains.open(); }
+}
+
+class MusicPlayCommand implements Command {
+    private MusicPlayer player;
+    public MusicPlayCommand(MusicPlayer player) { this.player = player; }
+    public void execute() { player.play(); }
+    public void undo() { player.stop(); }
+}
+
+class MusicStopCommand implements Command {
+    private MusicPlayer player;
+    public MusicStopCommand(MusicPlayer player) { this.player = player; }
+    public void execute() { player.stop(); }
+    public void undo() { player.play(); }
+}
+
+class MacroCommand implements Command {
+    private List<Command> commands;
+    public MacroCommand(List<Command> commands) { this.commands = commands; }
+    public void execute() { for (Command command : commands) command.execute(); }
+    public void undo() {
+        ListIterator<Command> it = commands.listIterator(commands.size());
+        while (it.hasPrevious()) it.previous().undo();
     }
+}
 
-    public class Light
-    {
-        public void On() => Console.WriteLine("Свет включен");
-        public void Off() => Console.WriteLine("Свет выключен");
-    }
+class RemoteControl {
+    private Map<Integer, Command> slots = new HashMap<>();
+    private Stack<Command> history = new Stack<>();
+    private Stack<Command> redoStack = new Stack<>();
 
-    public class AirConditioner
-    {
-        public void On() => Console.WriteLine("Кондиционер включен");
-        public void Off() => Console.WriteLine("Кондиционер выключен");
-    }
+    public void setCommand(int slot, Command command) { slots.put(slot, command); }
 
-    public class TV
-    {
-        public void On() => Console.WriteLine("Телевизор включен");
-        public void Off() => Console.WriteLine("Телевизор выключен");
-    }
-
-    public class Curtains
-    {
-        public void Open() => Console.WriteLine("Шторы открыты");
-        public void Close() => Console.WriteLine("Шторы закрыты");
-    }
-
-    public class MusicPlayer
-    {
-        public void Play() => Console.WriteLine("Музыка играет");
-        public void Stop() => Console.WriteLine("Музыка остановлена");
-    }
-
-    public class LightOnCommand : ICommand
-    {
-        private Light light;
-        public LightOnCommand(Light light) { this.light = light; }
-        public void Execute() => light.On();
-        public void Undo() => light.Off();
-    }
-
-    public class LightOffCommand : ICommand
-    {
-        private Light light;
-        public LightOffCommand(Light light) { this.light = light; }
-        public void Execute() => light.Off();
-        public void Undo() => light.On();
-    }
-
-    public class ACOnCommand : ICommand
-    {
-        private AirConditioner ac;
-        public ACOnCommand(AirConditioner ac) { this.ac = ac; }
-        public void Execute() => ac.On();
-        public void Undo() => ac.Off();
-    }
-
-    public class ACOffCommand : ICommand
-    {
-        private AirConditioner ac;
-        public ACOffCommand(AirConditioner ac) { this.ac = ac; }
-        public void Execute() => ac.Off();
-        public void Undo() => ac.On();
-    }
-
-    public class TVOnCommand : ICommand
-    {
-        private TV tv;
-        public TVOnCommand(TV tv) { this.tv = tv; }
-        public void Execute() => tv.On();
-        public void Undo() => tv.Off();
-    }
-
-    public class TVOffCommand : ICommand
-    {
-        private TV tv;
-        public TVOffCommand(TV tv) { this.tv = tv; }
-        public void Execute() => tv.Off();
-        public void Undo() => tv.On();
-    }
-
-    public class CurtainsOpenCommand : ICommand
-    {
-        private Curtains curtains;
-        public CurtainsOpenCommand(Curtains curtains) { this.curtains = curtains; }
-        public void Execute() => curtains.Open();
-        public void Undo() => curtains.Close();
-    }
-
-    public class CurtainsCloseCommand : ICommand
-    {
-        private Curtains curtains;
-        public CurtainsCloseCommand(Curtains curtains) { this.curtains = curtains; }
-        public void Execute() => curtains.Close();
-        public void Undo() => curtains.Open();
-    }
-
-    public class MusicPlayCommand : ICommand
-    {
-        private MusicPlayer player;
-        public MusicPlayCommand(MusicPlayer player) { this.player = player; }
-        public void Execute() => player.Play();
-        public void Undo() => player.Stop();
-    }
-
-    public class MusicStopCommand : ICommand
-    {
-        private MusicPlayer player;
-        public MusicStopCommand(MusicPlayer player) { this.player = player; }
-        public void Execute() => player.Stop();
-        public void Undo() => player.Play();
-    }
-
-    public class MacroCommand : ICommand
-    {
-        private List<ICommand> commands;
-        public MacroCommand(List<ICommand> commands) { this.commands = commands; }
-
-        public void Execute()
-        {
-            foreach (var command in commands)
-                command.Execute();
-        }
-
-        public void Undo()
-        {
-            for (int i = commands.Count - 1; i >= 0; i--)
-                commands[i].Undo();
+    public void pressButton(int slot) {
+        if (slots.containsKey(slot)) {
+            Command command = slots.get(slot);
+            command.execute();
+            history.push(command);
+            redoStack.clear();
+        } else {
+            System.out.println("Ошибка: слот " + slot + " пуст.");
         }
     }
 
-    public class RemoteControl
-    {
-        private Dictionary<int, ICommand> slots = new Dictionary<int, ICommand>();
-        private Stack<ICommand> history = new Stack<ICommand>();
-        private Stack<ICommand> redoStack = new Stack<ICommand>();
-
-        public void SetCommand(int slot, ICommand command)
-        {
-            slots[slot] = command;
-        }
-
-        public void PressButton(int slot)
-        {
-            if (slots.ContainsKey(slot))
-            {
-                ICommand command = slots[slot];
-                command.Execute();
-                history.Push(command);
-                redoStack.Clear();
-            }
-            else
-            {
-                Console.WriteLine($"Ошибка: слот {slot} пуст.");
-            }
-        }
-
-        public void Undo()
-        {
-            if (history.Count > 0)
-            {
-                ICommand command = history.Pop();
-                command.Undo();
-                redoStack.Push(command);
-            }
-            else
-            {
-                Console.WriteLine("Нет команд для отмены.");
-            }
-        }
-
-        public void Redo()
-        {
-            if (redoStack.Count > 0)
-            {
-                ICommand command = redoStack.Pop();
-                command.Execute();
-                history.Push(command);
-            }
-            else
-            {
-                Console.WriteLine("Нет команд для повтора.");
-            }
+    public void undo() {
+        if (!history.isEmpty()) {
+            Command command = history.pop();
+            command.undo();
+            redoStack.push(command);
+        } else {
+            System.out.println("Нет команд для отмены.");
         }
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            RemoteControl remote = new RemoteControl();
-
-            Light light = new Light();
-            AirConditioner ac = new AirConditioner();
-            TV tv = new TV();
-            Curtains curtains = new Curtains();
-            MusicPlayer player = new MusicPlayer();
-
-            remote.SetCommand(1, new LightOnCommand(light));
-            remote.SetCommand(2, new ACOnCommand(ac));
-            remote.SetCommand(3, new TVOnCommand(tv));
-            remote.SetCommand(4, new CurtainsOpenCommand(curtains));
-            remote.SetCommand(5, new MusicPlayCommand(player));
-
-            var partyCommands = new List<ICommand>
-            {
-                new LightOnCommand(light),
-                new ACOnCommand(ac),
-                new TVOnCommand(tv),
-                new MusicPlayCommand(player)
-            };
-            remote.SetCommand(6, new MacroCommand(partyCommands));
-
-            remote.PressButton(1);
-            remote.PressButton(2);
-            remote.PressButton(3);
-            remote.Undo();
-            remote.Redo();
-
-            Console.WriteLine("\nЗапуск макрокоманды:");
-            remote.PressButton(6);
-
-            Console.WriteLine("\nОтмена макрокоманды:");
-            remote.Undo();
-
-            Console.WriteLine("\nПроверка пустого слота:");
-            remote.PressButton(10);
+    public void redo() {
+        if (!redoStack.isEmpty()) {
+            Command command = redoStack.pop();
+            command.execute();
+            history.push(command);
+        } else {
+            System.out.println("Нет команд для повтора.");
         }
+    }
+}
+
+public class SmartHomeDemo {
+    public static void main(String[] args) {
+        RemoteControl remote = new RemoteControl();
+
+        Light light = new Light();
+        AirConditioner ac = new AirConditioner();
+        TV tv = new TV();
+        Curtains curtains = new Curtains();
+        MusicPlayer player = new MusicPlayer();
+
+        remote.setCommand(1, new LightOnCommand(light));
+        remote.setCommand(2, new ACOnCommand(ac));
+        remote.setCommand(3, new TVOnCommand(tv));
+        remote.setCommand(4, new CurtainsOpenCommand(curtains));
+        remote.setCommand(5, new MusicPlayCommand(player));
+
+        List<Command> partyCommands = Arrays.asList(
+            new LightOnCommand(light),
+            new ACOnCommand(ac),
+            new TVOnCommand(tv),
+            new MusicPlayCommand(player)
+        );
+        remote.setCommand(6, new MacroCommand(partyCommands));
+
+        remote.pressButton(1);
+        remote.pressButton(2);
+        remote.pressButton(3);
+        remote.undo();
+        remote.redo();
+
+        System.out.println("\nЗапуск макрокоманды:");
+        remote.pressButton(6);
+
+        System.out.println("\nОтмена макрокоманды:");
+        remote.undo();
+
+        System.out.println("\nПроверка пустого слота:");
+        remote.pressButton(10);
     }
 }
